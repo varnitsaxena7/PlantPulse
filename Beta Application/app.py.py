90a4373052b0962082d71cb0e6bccf810e4eb29a
varnitsaxena7 <118539746+varnitsaxena7@users.noreply.github.com>
2024-06-18 23:40:45 +0530

    Update app.py.py

--- Beta Application/app.py.py
+++ Beta Application/app.py.py
@@ -1,16 +1,22 @@
-import numpy as np
 import streamlit as st
 import cv2
 from keras.models import load_model
 import google.generativeai as genai
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
 genai.configure(api_key="AIzaSyBOisPhVp7vcjWXkcyU1KEQEiUvdhCiBIE")
+
 def gemini_pro_response(user_prompt):
     gemini_pro_model = genai.GenerativeModel("gemini-pro")
     response = gemini_pro_model.generate_content(user_prompt)
     result = response.text
     return result
+
 model = load_model('plant_disease.h5')
 CLASS_NAMES = ['Corn-Common_rust', 'Potato-Early_blight', 'Tomato-Bacterial_spot']
+
 st.markdown(
     """
     <style>
@@ -60,18 +66,18 @@ st.markdown("<h1 class='title'>Plant Disease Detection</h1>", unsafe_allow_html=
 st.markdown("Upload an image of the plant leaf", unsafe_allow_html=True)
 
 plant_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-it=st.checkbox("Prevention and Cure Details")
+it = st.checkbox("Prevention and Cure Details")
+
 if st.button('Predict', key='predict_button'):
 
     if plant_image is not None:
-        file_bytes = np.asarray(bytearray(plant_image.read()), dtype=np.uint8)
-        opencv_image = cv2.imdecode(file_bytes, 1)
-
+        image = Image.open(plant_image)
+        opencv_image = cv2.cvtColor(tf.keras.preprocessing.image.img_to_array(image), cv2.COLOR_RGB2BGR)
+        
         st.image(opencv_image, channels="BGR", caption="Uploaded Image", width=300)
 
         resized_image = cv2.resize(opencv_image, (256, 256))
-
-        input_image = np.expand_dims(resized_image, axis=0)
+        input_image = resized_image.reshape((1, 256, 256, 3))
 
         Y_pred = model.predict(input_image)
         predicted_class = CLASS_NAMES[np.argmax(Y_pred)]
